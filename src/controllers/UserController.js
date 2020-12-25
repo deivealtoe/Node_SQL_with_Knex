55deb65f0966813074980c35327957c0e6d9bbb5
Deivisson Altoé <deivealtoe@gmail.com>
2020-12-25 09:43:23 -0300

    Finalizado CRUD de usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,45 @@
-const knex = require('../database');
+const knex = require('../database/index');
 
 module.exports = {
     async index(request, response) {
         const results = await knex('users');
 
         return response.json(results);
+    },
+    async create(request, response, next) {
+        try {
+            const { username } = request.body;
+
+            await knex('users').insert({
+                username
+            });
+
+            return response.status(201).send();
+        } catch (error) {
+            next(error);
+        }
+    },
+    async update(request, response, next) {
+        try {
+            const { username } = request.body;
+            const { id } = request.params;
+
+            await knex('users').update({ username }).where({ id });
+
+            return response.status(200).send();
+        } catch (error) {
+            next(error);
+        }
+    },
+    async delete(request, response, next) {
+        try {
+            const { id } = request.params;
+
+            await knex('users').where({ id }).del();
+
+            return response.status(200).send();
+        } catch (error) {
+            next(error);
+        }
     }
 }
--- src/server.js
+++ src/server.js
@@ -3,7 +3,25 @@ const routes = require('./routes');
 
 const server = express();
 
+server.use(express.json());
+
 server.use(routes);
+
+// Not found
+server.use((request, response, next) => {
+    const error = new Error('Not found');
+
+    error.status = 404;
+
+    next(error);
+});
+
+// Catch all - middleware
+server.use((error, request, response, next) => {
+    response.status(error.status || 500);
+
+    response.json({ error: error.message });
+});
 
 const PORT = 3333;
 const HOST = '0.0.0.0';
